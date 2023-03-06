33c49ef49c017353f65bf9d774923f6d1072088c
jamie macdonald <jamie.macdonald@n-able.com>
2023-03-06 18:32:04 +0000

    windows env vars
    Signed-off-by: jamie macdonald <jamie.macdonald@n-able.com>

--- UdemyGRPCK6Complete/com.github.grpc/src/main/java/brewers/delight/CoffeeShopServer.java
+++ UdemyGRPCK6Complete/com.github.grpc/src/main/java/brewers/delight/CoffeeShopServer.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class CoffeeShopServer {
     public static void main(String[] args) throws IOException, InterruptedException
     {
-        Server server = ServerBuilder.forPort(50052)
+        Server server = ServerBuilder.forPort(50055)
                 .addService(new CoffeeShopServiceImpl())
                 .build();
         System.out.println("server starting....");
